8dfe899c1bbe6bfe38ba68faa0732a5774d7cbbf
nemo-omen <nemo.omen@gmail.com>
2023-10-09 23:41:25 -0500

    More fun with the server prototype

--- prototypes/server-Jeff/src/app.py
+++ prototypes/server-Jeff/src/app.py
@@ -1,13 +1,39 @@
-from emmett import App
-
+from emmett import App, Field, Form, request, session, url
+from emmett.sessions import SessionManager
+from controllers.dashboard_controller import DashboardController
 app = App(__name__)
 
 
+dash_controller = DashboardController()
+
+static = url('static/')  # defining a static route is so very simple
+
+app.pipeline = [SessionManager.cookies('secretkey')]
+
+
 @app.route('/')
-async def hello():
-    return "Hello world"
+async def home():
+    session.counter = (session.counter or 0) + 1
+    return {'count': session.counter}
 
 
-@app.route('/<str:msg>')
-async def echo(msg):
-    return dict(message=msg)
+@app.route('/dashboard')
+async def dashboard():  # okay, so the method matches the template name, fun
+    return {'name': 'Pie Hole'}
+
+
+@app.route('/form')
+async def form():
+    simple_form = await Form({
+        'name': Field()
+    })
+    if simple_form.accepted:
+        print('Okay')
+    return dict(form=simple_form)
+
+
+@app.route('/static-form')
+async def static_form():
+    params = await request.body_params
+    input_name = params.name or 'Nobody!'
+    return {'name': input_name}
---
+++ prototypes/server-Jeff/src/controllers/dashboard_controller.py
@@ -0,0 +1,11 @@
+class DashboardController():
+    # def __init__(self, app, analysis_service) -> None:
+    def __init__(self) -> None:
+        # self.analysis_service = analysis_service
+        pass
+
+    def get_overview(self) -> None:
+        return "Pretend this is an overview..."
+        # current_accuracy = self.analysis_service.get_latest_accuracy()
+        # current_forecast = self.analysis_service.get_latest_forecast()
+        # current_optimization = self.analysis_service.get_latest_optimization()
