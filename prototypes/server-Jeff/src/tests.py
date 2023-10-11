a26bcd973d345cdd7d7b2aebcc8791d4862f5703
nemo-omen <nemo.omen@gmail.com>
2023-10-11 07:49:25 -0500

    Remove test

--- prototypes/server-Jeff/src/tests.py
+++ prototypes/server-Jeff/src/tests.py
@@ -10,10 +10,3 @@ def client():
 def test_route_not_none(client):
     rv = client.get('/')
     assert rv is not None
-
-
-def test_route_returns_dict_with_number(client):
-    # add a comment
-    rv = client.get('/')
-    print(rv)
-    assert 'count' in rv.keys
