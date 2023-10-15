dad715c31f0e1196c4c4c13164b0f71ecf496b52
nemo-omen <nemo.omen@gmail.com>
2023-10-15 18:00:02 -0500

    Base views done (ish)

--- src/controllers/data.py
+++ src/controllers/data.py
@@ -1,5 +1,7 @@
+import uuid
 from app import app
-from emmett import response
+from emmett import request, response
+from emmett.helpers import stream_file
 
 data = app.module(__name__, 'data', url_prefix='data', template_folder='pages/data')
 
@@ -10,4 +12,11 @@ async def index():
 
 @data.route('/upload', methods='post')
 async def upload():
-    return {}
+    response.meta.title = 'Upload | ISS Consumables'
+    files = await request.files
+    file = files.file
+    id = str(uuid.uuid4())
+    type = file.content_type.split('/',1)[0]
+    ext = file.content_type.split('/',1)[1]
+    await file.save(f"storage/{id}.{ext}")
+    return {'name': file.filename, 'ext': ext, 'type': type}
