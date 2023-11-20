102a69e2ce452ed59c600cf4fa2b3668b5021256
nemo-omen <nemo.omen@gmail.com>
2023-11-20 10:32:50 -0600

    Add database table views

--- server/barrios/data/services.py
+++ server/barrios/data/services.py
@@ -1,9 +1,11 @@
 from csv import DictReader
 from cases import to_snake
+from django.db.models import Model
 from common.result import Result
 from django.db import connections
 import pandas as pd
-
+import numpy as np
+import subprocess
 
 from .models import (
     Category,
@@ -28,20 +30,14 @@ class DataService:
         if filepath is not None:
             self.file = filepath
 
-    def copy_csv_to_db(self, table_name):
-        SQL_STATEMENT = """
-            COPY %s FROM STDIN WITH
-                CSV
-                HEADER
-                DELIMITER AS ','
-        """
-        conn = connections["default"]
-        cursor = conn.cursor()
-        with open(self.file, "r") as fp:
-            next(fp)
-            cursor.copy_expert(sql=SQL_STATEMENT % table_name, file=fp)
-            conn.commit()
-            cursor.close()
+    def get_num_lines(self, csv_path):
+        chunk = 1024 * 1024  # Process 1 MB at a time.
+        f = np.memmap(csv_path)
+        num_newlines = sum(
+            np.sum(f[i : i + chunk] == ord("\n")) for i in range(0, len(f), chunk)
+        )
+        del f
+        return num_newlines
 
     def keys_to_snake(self, model_dict):
         new_dict = {}
@@ -104,47 +100,73 @@ class DataService:
                 "error": "There was an error saving your data.",
             }
 
-    def read_and_insert_csv(self, model_name: str, csv_path: str) -> Result:
-        # if model_name == 'InventoryMgmtSystemConsumables':
-
-        with open(csv_path, "r", newline="", encoding="utf-8-sig") as file:
-            reader = DictReader(file)
-            for row in reader:
-                converted = self.keys_to_snake(row)
-                result = None
-
-                if model_name == "ImsConsumablesCategoryLookup":
-                    result = ImsConsumablesCategoryLookup.objects.create(**converted)
-                # elif model_name == "InventoryMgmtSystemConsumables":
-                #     result = InventoryMgmtSystemConsumables.objects.create(**converted)
-                elif model_name == "IssFlightPlan":
-                    result = IssFlightPlan.objects.create(**converted)
-                elif model_name == "IssFlightPlanCrew":
-                    result = IssFlightPlanCrew.objects.create(**converted)
-                elif model_name == "IssFlightPlanCrewNationalityLookup":
-                    result = IssFlightPlanCrewNationalityLookup.objects.create(
-                        **converted
-                    )
-                elif model_name == "RatesDefinition":
-                    result = RatesDefinition.objects.create(**converted)
-                elif model_name == "RsaConsumableWaterSummary":
-                    result = RsaConsumableWaterSummary.objects.create(**converted)
-                elif model_name == "TankCapacityDefinition":
-                    result = TankCapacityDefinition.objects.create(**converted)
-                elif model_name == "ThresholdsLimitsDefinition":
-                    result = ThresholdsLimitsDefinition.objects.create(**converted)
-                elif model_name == "UsRsWeeklyConsumableGasSummary":
-                    result = UsRsWeeklyConsumableGasSummary.objects.create(**converted)
-                elif model_name == "UsWeeklyConsumableWaterSummary":
-                    result = UsWeeklyConsumableWaterSummary.objects.create(**converted)
-                if result is not None:
-                    return {"ok": True, "value": None, "error": None}
-                else:
-                    return {
-                        "ok": False,
-                        "value": None,
-                        "error": "Unable to save records",
-                    }
-
     def get_uploaded_file(self, csv_path):
-        return pd.read_csv(csv_path, index_col=False, keep_default_na=False)
+        # TODO: Return a result with the dataframe,
+        # whether the dataframe is a truncated preview
+        # and the total number of rows  in the file
+        num_lines = self.get_num_lines(csv_path)
+        print(f"num_lines: {num_lines}")
+
+        if num_lines > 10000:
+            df = pd.read_csv(
+                csv_path, index_col=False, keep_default_na=False, chunksize=10000
+            )
+            return next(df)
+        else:
+            df = pd.read_csv(csv_path, index_col=False, keep_default_na=False)
+            return df
+
+    def get_model_by_ref(self, ref: str) -> Model:
+        model = None
+        if ref == "ims_consumables":
+            model = InventoryMgmtSystemConsumables
+        elif ref == "category_lookup":
+            model = ImsConsumablesCategoryLookup
+        elif ref == "flight_plan":
+            model = IssFlightPlan
+        elif ref == "crew_flight_plan":
+            model = IssFlightPlanCrew
+        elif ref == "crew_nationality_lookup":
+            model = IssFlightPlanCrewNationalityLookup
+        elif ref == "us_water_summary":
+            model = UsWeeklyConsumableWaterSummary
+        elif ref == "rsa_water_summary":
+            model = RsaConsumableWaterSummary
+        elif ref == "weekly_gas_summary":
+            model = UsRsWeeklyConsumableGasSummary
+        elif ref == "rates_definitions":
+            model = RatesDefinition.objects.all()
+        elif ref == "tank_capacities":
+            model = TankCapacityDefinition
+        elif ref == "thresholds_and_limits":
+            model = ThresholdsLimitsDefinition
+        return model
+
+    def get_data_by_ref(self, ref: str) -> Result:
+        results = None
+        if ref == "ims_consumables":
+            results = InventoryMgmtSystemConsumables.objects.all().order_by("-datedim")
+        elif ref == "category_lookup":
+            results = ImsConsumablesCategoryLookup.objects.all().order_by("category_id")
+        elif ref == "flight_plan":
+            results = IssFlightPlan.objects.all().order_by("datedim")
+        elif ref == "crew_flight_plan":
+            results = IssFlightPlanCrew.objects.all().order_by("datedim")
+        elif ref == "crew_nationality_lookup":
+            results = IssFlightPlanCrewNationalityLookup.objects.all()
+        elif ref == "us_water_summary":
+            results = UsWeeklyConsumableWaterSummary.objects.all().order_by("date")
+        elif ref == "rsa_water_summary":
+            results = RsaConsumableWaterSummary.objects.all().order_by("report_date")
+        elif ref == "weekly_gas_summary":
+            results = UsRsWeeklyConsumableGasSummary.objects.all().order_by("date")
+        elif ref == "rates_definitions":
+            results = RatesDefinition.objects.all()
+        elif ref == "tank_capacities":
+            results = TankCapacityDefinition.objects.all()
+        elif ref == "thresholds_and_limits":
+            results = ThresholdsLimitsDefinition.objects.all()
+        if results is not None:
+            return {"ok": True, "value": results, "error": None}
+        else:
+            return {"ok": False, "value": None, "error": "No results found"}
--- server/barrios/data/utils/FieldFileCsvHelper.py
+++ server/barrios/data/utils/FieldFileCsvHelper.py
@@ -6,11 +6,16 @@ class FieldFileCsvHelper:
         # NOTE: parse_dates?
         # (see: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.read_csv.html#pandas-read-csv)
         df = pd.read_csv(
-            filepath, header=0, names=fieldnames, index_col=False, keep_default_na=False
+            filepath,
+            header=0,
+            names=fieldnames,
+            index_col=False,
+            keep_default_na=False,
+            low_memory=False,
         )
         df = df.loc[:, ~df.columns.str.match("Unnamed")]
         file_result = df.to_csv(filepath, index=False, chunksize=5000)
-        print(file_result)
+        # TODO: Return result object
 
     def get_csv_header(self, filepath):
         df = pd.read_csv(filepath, index_col=False, nrows=0)
