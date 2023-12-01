28bad59f0cd1f0844dba5390615c9edb2f0f2606
nemo-omen <nemo.omen@gmail.com>
2023-11-30 21:36:26 -0600

    Simple chart transitions & water usage chart

---
+++ server/barrios/usage/charts.py
@@ -0,0 +1,51 @@
+import pandas as pd
+from plotly.offline import plot
+import plotly.express as px
+import plotly.graph_objs as go
+from math import floor
+
+
+def create_usage_chart(usage_obj, with_title=True):
+    consumable_name = usage_obj["consumable_name"]
+    df = usage_obj["df"]
+    print(df)
+    fig = go.Figure()
+
+    fig.update_layout(
+        paper_bgcolor="#091D41",
+        plot_bgcolor="#091D41",
+        title_font_color="#ffffff",
+        legend_font_color="#ffffff",
+        margin=dict(t=32, r=8, b=8, l=8),
+        font=dict(color="#ffffff"),
+        hovermode="x",
+        newshape_layer="below",
+    )
+
+    if with_title:
+        fig.update_layout(title={"text": consumable_name.upper()})
+
+    fig.add_trace(
+        go.Scatter(
+            x=list(df["date"]),
+            y=list(df["actual_value"]),
+            name="Actual",
+            line=dict(color="#dade35", width=2),
+        )
+    )
+    fig.add_trace(
+        go.Scatter(
+            x=list(df["date"]),
+            y=list(df["predicted_value"]),
+            name="Predicted",
+            line=dict(color="#ffffff", width=2),
+        )
+    )
+    line_plot = fig.to_html(
+        config={"displayModeBar": False, "responsive": True},
+        full_html=False,
+        # default_height=600,
+        # include_plotlyjs=False,
+        include_mathjax=False,
+    )
+    return line_plot
--- server/barrios/usage/views.py
+++ server/barrios/usage/views.py
@@ -1,12 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from random import randint
-from .forms import UsageForm
+from usage.forms import UsageForm
+from usage.charts import create_usage_chart
 from common.conditionalredirect import conditionalredirect
-from datetime import date
+from datetime import date, datetime
+from common.date_helpers import get_date_object
+import pandas as pd
 from data.models import (
     InventoryMgmtSystemConsumables,
+    IssFlightPlanCrew,
+    IssFlightPlanCrewNationalityLookup,
     RatesDefinition,
+    UsRsWeeklyConsumableGasSummary,
     UsWeeklyConsumableWaterSummary,
 )
 
@@ -29,15 +35,55 @@ def analyze(request):
     if request.method == "POST":
         form = UsageForm(request.POST)
         if form.is_valid():
-            start_date = form.cleaned_data["start_date"]
-            end_date = form.cleaned_data["end_date"]
+            start_date = get_date_object(form.cleaned_data["start_date"])
+            end_date = get_date_object(form.cleaned_data["end_date"])
             consumable_name = form.cleaned_data["consumable_name"]
-            print(
-                f"start_date: {start_date}, end_date: {end_date}, consumable_name: {consumable_name}"
-            )
-
             # get list of consumables
-            rates_definitions_values = RatesDefinition.objects.all().values()
+            rate_definition_value = RatesDefinition.objects.filter(
+                affected_consumable=consumable_name
+            ).values()
+
+            actual_usage_values = []
+            if consumable_name == "Water":
+                actual_usage_values = UsWeeklyConsumableWaterSummary.objects.filter(
+                    date__range=[start_date, end_date]
+                ).values()
+            elif (
+                consumable_name == "Oxygen"
+                or consumable_name == "Nitrogen"
+                or consumable_name == "Air"
+            ):
+                actual_usage_values = UsRsWeeklyConsumableGasSummary.objects.filter(
+                    date__range=[start_date, end_date]
+                ).values()
+
+            usage_df: pd.DataFrame | None = None
+            usage_chart = None
+
+            if len(actual_usage_values) > 0:
+                usage_df = pd.DataFrame(actual_usage_values)
+
+            if usage_df is not None:
+                if consumable_name == "Water":
+                    usage_df = usage_df.drop(
+                        columns=[
+                            "corrected_potable_l",
+                            "corrected_technical_l",
+                            "resupply_potable_l",
+                            "resupply_technical_l",
+                        ]
+                    )
+                    usage_df = usage_df.rename(
+                        columns={
+                            "corrected_total_l": "actual_value",
+                            "corrected_predicted_l": "predicted_value",
+                        }
+                    )
+
+                    usage_chart = create_usage_chart(
+                        {"consumable_name": consumable_name, "df": usage_df}
+                    )
+
             # for each:
             # - if consumable has category_id
             #   query InventoryMgmtSystemConsumables
@@ -48,9 +94,17 @@ def analyze(request):
             #   and calculate difference as a time series
             #     - This can be displayed on a line chart with the actual
             #       number of items(?)
-            return render(
-                request, "pages/usage/result.html", {"start_date": start_date}
-            )
+            # print(usage_chart)
+            if usage_chart:
+                return render(
+                    request, "pages/usage/result.html", {"usage_chart": usage_chart}
+                )
+            else:
+                return render(
+                    request,
+                    "pages/usage/result.html",
+                    {"usage_chart": usage_df.to_html()},
+                )
         else:
             return render(
                 request, "pages/usage/result.html", {"error": "Invalid selection"}
