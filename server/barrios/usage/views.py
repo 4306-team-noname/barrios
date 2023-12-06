ea8bc95e9173cffb75b3a339f87504933bc7c0a5
nemo-omen <nemo.omen@gmail.com>
2023-12-05 23:05:20 -0600

    Gases and water usage integration works

--- server/barrios/usage/views.py
+++ server/barrios/usage/views.py
@@ -55,89 +55,13 @@ def analyze(request):
             # consumable_name can just be cast directly
             consumable_name = str(consumable_name_obj)
 
+            # Instantiate a Rater to handle our calculations
+            # and generate our graphics
             rater = Rater(consumable_name, start_date_obj, end_date_obj)
             rate_object = rater.rate()
-            print(rate_object)
-            # get list of consumables
-            rate_definition_value = RatesDefinition.objects.filter(
-                affected_consumable=consumable_name,
-                type="usage",
-            ).values()
+            actual_usage_values = rater.create_usage_dataframe()
+            usage_chart = rater.plot()
 
-            actual_usage_values = []
-            if consumable_name == "Water":
-                actual_usage_values = UsWeeklyConsumableWaterSummary.objects.filter(
-                    date__range=[start_date, end_date]
-                ).values()
-            elif (
-                consumable_name == "Oxygen"
-                or consumable_name == "Nitrogen"
-                or consumable_name == "Air"
-            ):
-                actual_usage_values = UsRsWeeklyConsumableGasSummary.objects.filter(
-                    date__range=[start_date, end_date]
-                ).values()
-            else:
-                consumable_cat = RatesDefinition.objects.get(
-                    affected_consumable=consumable_name
-                )
-                actual_usage_values = InventoryMgmtSystemConsumables.objects.filter(
-                    category_id=consumable_cat.category
-                ).values(
-                    "datedim",
-                    "ims_id",
-                    "english_name",
-                    "quantity",
-                    "status",
-                    "category",
-                    "category_name",
-                )
-                # print(actual_usage_values)
-
-                # return render(
-                #     request,
-                #     "pages/usage/result.html",
-                #     {"error_message": "No analysis for that consumable yet."},
-                # )
-
-            usage_df: pd.DataFrame | None = None
-            usage_chart = None
-
-            if len(actual_usage_values) > 0:
-                usage_df = pd.DataFrame(actual_usage_values)
-
-            if usage_df is not None:
-                if consumable_name == "Water":
-                    usage_df = usage_df.drop(
-                        columns=[
-                            "corrected_potable_l",
-                            "corrected_technical_l",
-                            "resupply_potable_l",
-                            "resupply_technical_l",
-                        ]
-                    )
-                    usage_df = usage_df.rename(
-                        columns={
-                            "corrected_total_l": "actual_value",
-                            "corrected_predicted_l": "predicted_value",
-                        }
-                    )
-
-                    usage_chart = create_usage_chart(
-                        {"consumable_name": consumable_name, "df": usage_df}
-                    )
-
-            # for each:
-            # - if consumable has category_id
-            #   query InventoryMgmtSystemConsumables
-            #   and begin calculation
-            # - else, determine if it is water, gas, etc.
-            #   and calculate usage from appropriate model
-            # - also, get assumed usage rate from RatesDefinitions
-            #   and calculate difference as a time series
-            #     - This can be displayed on a line chart with the actual
-            #       number of items(?)
-            # print(usage_chart)
             if usage_chart:
                 return render(
                     request,
